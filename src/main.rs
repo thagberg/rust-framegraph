753cd954b72961d9aa75c70475d63522e116487e
Tim Hagberg <timothy.m.hagberg@gmail.com>
2022-01-10 20:34:19 -0500

    Create instance

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use winit::event_loop::{EventLoop, ControlFlow};
 use winit::window::Window;
 
 use ash::vk;
+use ash::{Device, Instance};
+use ash::extensions::{
+    ext::DebugUtils,
+    khr::{Surface, Swapchain},
+};
 
 use std::ffi::{CStr, CString};
 use std::ptr;
@@ -13,16 +18,17 @@ const WINDOW_WIDTH: u32 = 1400;
 
 struct WindowedApp {
     // instance: ash::Instance
+    instance: Instance
 }
 
 impl WindowedApp {
-    pub fn new() -> WindowedApp {
+    pub fn new(event_loop: &EventLoop<()>, window: &Window) -> WindowedApp {
         let entry = ash::Entry::linked();
         // let instance = WindowedApp::create_instance(&entry, &None);
-        WindowedApp::create_instance(&entry, &None);
+        let instance = WindowedApp::create_instance(&entry, window);
 
         WindowedApp {
-            // instance
+            instance
         }
     }
 
@@ -35,35 +41,41 @@ impl WindowedApp {
     }
 
     // fn create_instance(entry: &ash::Entry, window: &Option<Window>) -> ash::Instance {
-    fn create_instance(entry: &ash::Entry, window: &Option<Window>) {
+    fn create_instance(entry: &ash::Entry, window: &Window) -> Instance{
+        let engine_name = CString::new("Framegraph").unwrap();
+        let app_name = CString::new(WINDOW_TITLE).unwrap();
         let app_info = vk::ApplicationInfo {
             s_type: vk::StructureType::APPLICATION_INFO,
             p_next: std::ptr::null(),
-            p_application_name: CString::new(WINDOW_TITLE).unwrap().as_ptr(),
+            p_application_name: app_name.as_ptr(),
             application_version: vk::make_api_version(0, 0, 1, 0),
-            p_engine_name: CString::new("Framegraph").unwrap().as_ptr(),
+            p_engine_name: engine_name.as_ptr(),
             engine_version: vk::make_api_version(0, 0, 1, 0),
             api_version: vk::make_api_version(0, 1, 2, 198)
         };
 
-        let layer_names = [CString::new("VK_LAYER_KHRONOS_validation").unwrap().as_ptr()];
+        let validation_layer = CString::new("VK_LAYER_KHRONOS_validation").unwrap();
+        let layer_names = [validation_layer.as_ptr()];
 
-        //let extension_names = [ash_window::enumerate_required_extensions()]
-        // let extension_names = match window {
-        //     Some(window) => [ash_window::enumerate_required_extensions(window).unwrap()],
-        //     None => []
-        // };
-        let mut extension_names: Vec<&CStr>;
-        match window {
-            Some(window) => extension_names = ash_window::enumerate_required_extensions(window).unwrap(),
-            None => {},
-        }
+        let mut extension_names_raw = ash_window::enumerate_required_extensions(&window).unwrap()
+            .iter()
+            .map(|extension| extension.as_ptr())
+            .collect::<Vec<_>>();
+        extension_names_raw.push(DebugUtils::name().as_ptr());
 
         let create_info = vk::InstanceCreateInfo::builder()
             .application_info(&app_info)
             .enabled_layer_names(&layer_names)
-            .enabled_extension_names(&extension_names[..]);
+            .enabled_extension_names(&extension_names_raw);
 
+        let mut instance;
+        unsafe {
+            instance = entry
+                .create_instance(&create_info, None)
+                .expect("Failed to create Vulkan Instance");
+        }
+
+        instance
     }
 
     fn main_loop(event_loop: EventLoop<()>) {
@@ -84,8 +96,7 @@ impl WindowedApp {
                                         },
                                         _ => {},
                                     }
-                                },
-                                _ => {},
+                                }
                             }
                         }
                         _ => {},
@@ -101,5 +112,6 @@ fn main() {
     println!("Hello, world!");
     let event_loop = EventLoop::new();
     let window = WindowedApp::init_window(&event_loop);
-    WindowedApp::main_loop(event_loop);
+    //WindowedApp::main_loop(event_loop, window);
+    let app = WindowedApp::new(&event_loop, &window);
 }
