73629e7eb0cea7cf59a046c2d96e7eb39f35f34e
Tim Hagberg <timothy.m.hagberg@gmail.com>
2022-02-07 00:51:37 -0500

    Moving device initialization to RenderContext

--- src/api_types/device.rs
+++ src/api_types/device.rs
@@ -1,5 +1,19 @@
 use ash::vk;
 
+pub struct PhysicalDeviceWrapper {
+    physical_device: vk::PhysicalDevice
+}
+
+impl PhysicalDeviceWrapper {
+    pub fn new(physical_device: vk::PhysicalDevice) -> PhysicalDeviceWrapper {
+        PhysicalDeviceWrapper {
+            physical_device
+        }
+    }
+
+    pub fn get(&self) -> vk::PhysicalDevice { self.physical_device }
+}
+
 pub struct DeviceWrapper {
     device: ash::Device
 }
