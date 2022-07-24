9327dbd7db6236ea7ce3c398c9f8cd6687864121
Tim Hagberg <timothy.m.hagberg@gmail.com>
2022-07-24 18:48:35 -0400

    Fix all compiler warnings

--- src/context/pipeline.rs
+++ src/context/pipeline.rs
@@ -4,7 +4,7 @@ use std::hash::{Hash, Hasher};
 
 use ash::vk;
 
-use crate::context::shader::{ShaderManager, ShaderModule};
+use crate::context::shader::ShaderManager;
 use crate::context::render_context::RenderContext;
 
 #[derive(Copy, Clone)]
