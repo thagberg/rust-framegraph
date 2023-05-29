8e2bbccd78b419103de0b9c6e0f278745916c81e
Tim Hagberg <timothy.m.hagberg@gmail.com>
2023-05-29 18:54:47 -0400

    WIP: Adding compute support

---
+++ passes/shaders/blur.compute.glsl
@@ -0,0 +1,8 @@
+#version 450
+
+layout(set=0, binding=0) uniform image2D source;
+
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+void main() {
+
+}
--- passes/src/blur.rs
+++ passes/src/blur.rs
@@ -2,17 +2,58 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 use ash::vk;
+use gpu_allocator::MemoryLocation;
 
-use context::api_types::device::DeviceResource;
-use framegraph::graphics_pass_node::GraphicsPassNode;
+use context::api_types::device::{DeviceResource, DeviceWrapper};
+use context::api_types::image::ImageCreateInfo;
 use context::vulkan_render_context::VulkanRenderContext;
+use framegraph::binding::{BindingInfo, BindingType, ImageBindingInfo, ResourceBinding};
+use framegraph::compute_pass_node::ComputePassNode;
+use framegraph::pass_type::PassType;
+
+const BLUR_TARGET_CREATE_INFO: ImageCreateInfo = ImageCreateInfo::new(
+    Default::default(),
+    "blur_target".to_string());
 
 pub fn generate_pass(
+    device: Rc<RefCell<DeviceWrapper>>,
     source: Rc<RefCell<DeviceResource>>
-) -> GraphicsPassNode {
+) -> PassType {
+    
+    let source_binding = ResourceBinding {
+        resource: source.clone(),
+        binding_info: BindingInfo {
+            binding_type: BindingType::Image(ImageBindingInfo {
+                layout: vk::ImageLayout::SHADER_READ_ONLY_OPTIMAL
+            }),
+            set: 0,
+            slot: 0,
+            stage: vk::PipelineStageFlags::COMPUTE_SHADER,
+            access: vk::AccessFlags::SHADER_READ
+        }
+    };
 
+    let blur_target = Rc::new(RefCell::new(DeviceWrapper::create_image(
+        device,
+        &BLUR_TARGET_CREATE_INFO,
+        MemoryLocation::Unknown)));
 
-    GraphicsPassNode::builder("blur".to_string())
+    let target_binding = ResourceBinding {
+        resource: blur_target,
+        binding_info: BindingInfo {
+            binding_type: BindingType::Image(ImageBindingInfo {
+                layout: vk::ImageLayout::GENERAL
+            }),
+            set: 0,
+            slot: 0,
+            stage: Default::default(),
+            access: Default::default()
+        }
+    };
+
+    let pass_node = ComputePassNode::builder("blur".to_string())
+        .input(source_binding)
+        .output(target_binding)
         .fill_commands(Box::new(
             move |render_ctx: &VulkanRenderContext,
                   command_buffer: &vk::CommandBuffer | {
@@ -20,5 +61,7 @@ pub fn generate_pass(
             }
         ))
         .build()
-        .expect("Failed to create blur passnode")
+        .expect("Failed to create blur passnode");
+
+    PassType::Compute(pass_node)
 }
