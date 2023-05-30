be2f4349382f370dd48f53e744d1294039c58adf
Tim Hagberg <timothy.m.hagberg@gmail.com>
2023-05-29 21:13:05 -0400

    Begin adding support for more pass node types

---
+++ framegraph/src/pass_type.rs
@@ -0,0 +1,28 @@
+use std::cell::RefCell;
+use std::ops::Deref;
+use std::rc::Rc;
+use ash::vk;
+use context::api_types::device::DeviceResource;
+use context::vulkan_render_context::VulkanRenderContext;
+use crate::attachment::AttachmentReference;
+use crate::binding::ResourceBinding;
+use crate::graphics_pass_node::GraphicsPassNode;
+use crate::pass_node::PassNode;
+use crate::pipeline::PipelineDescription;
+
+pub enum PassType {
+    Graphics(GraphicsPassNode)
+}
+
+// TODO: this could definitely be handled as a macro
+impl Deref for PassType {
+    type Target = dyn PassNode;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            PassType::Graphics(gn) => {
+                gn as &dyn PassNode
+            }
+        }
+    }
+}
