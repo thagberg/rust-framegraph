b67d54703df8008cfab991c2ab5d4e0988d0653c
Tim Hagberg <timothy.m.hagberg@gmail.com>
2023-09-02 23:52:00 -0400

    Adding examples binary project

---
+++ framegraph-examples/src/main.rs
@@ -0,0 +1,18 @@
+use imgui_winit_support::{WinitPlatform};
+
+struct WindowedVulkanApp {
+    window: winit::window::Window,
+    platform: WinitPlatform,
+
+
+}
+
+impl WindowedVulkanApp {
+
+}
+
+fn main() {
+    // create app
+
+    // app.run
+}
