36fb2825279c4ed4ec4d0057644480f4c2fd721c
Tim Hagberg <timothy.m.hagberg@gmail.com>
2023-09-03 19:15:04 -0400

    Mapping out draw_frame implementation

--- framegraph-examples/src/main.rs
+++ framegraph-examples/src/main.rs
@@ -77,8 +77,43 @@ impl WindowedVulkanApp {
         }
     }
 
-    pub fn draw_frame(&self) {
+    pub fn draw_frame(&mut self) {
+        // wait for fence if necessary (can we avoid this using just semaphores?)
 
+        // get swapchain image for this frame
+        // TODO: add semaphore to this
+        let (swapchain_image, image_index) = self.render_context.get_swapchain().as_ref().unwrap()
+            .acquire_next_image(None, None, None);
+
+        // begin commandbuffer
+        let command_buffer = self.render_context.get_graphics_command_buffer(image_index as usize);
+        unsafe {
+            self.render_context.get_device().borrow().get().reset_command_buffer(
+                command_buffer,
+                vk::CommandBufferResetFlags::empty())
+                .expect("Failed to reset command buffer");
+            let begin_info = vk::CommandBufferBeginInfo::builder()
+                .flags(vk::CommandBuferUsageFlags::SIMULTANEOUS_USE)
+                .build();
+            self.render_context.get_device().borrow().get().begin_command_buffer(command_buffer, &begin_info)
+                .expect("Failed to begin recording command buffer");
+        }
+
+        // update imgui UI
+        let imgui_draw_data = {
+            let ui = self.imgui.new_frame();
+            ui.text("Testing UI");
+
+            self.imgui.render()
+        };
+
+        // prepare framegraph
+
+        // queue submit
+
+        // prepare present
+
+        // queue present (wait on semaphores)
     }
 
     pub fn run(mut self, event_loop: EventLoop<()>) -> Result<u32, &'static str>{
