c8186299211ee1b0f4aec30bccc98d3a8aebc690
Tim Hagberg <timothy.m.hagberg@gmail.com>
2023-09-18 00:51:48 -0400

    Experiment with present pass node

---
+++ framegraph/src/present_pass_node.rs
@@ -0,0 +1,63 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use ash::vk;
+use ash::vk::CommandBuffer;
+use context::api_types::device::DeviceResource;
+use context::vulkan_render_context::VulkanRenderContext;
+use crate::pass_node::PassNode;
+
+pub struct PresentPassNode {
+    pub swapchain_image: Rc<RefCell<DeviceResource>>,
+    name: String
+}
+
+#[derive(Default)]
+pub struct PresentPassNodeBuilder {
+    name: String,
+    swapchain_image: Option<Rc<RefCell<DeviceResource>>>
+}
+
+impl PresentPassNode {
+    pub fn builder(name: String) -> PresentPassNodeBuilder {
+        PresentPassNodeBuilder {
+            name,
+            ..Default::default()
+        }
+    }
+}
+
+impl PresentPassNodeBuilder {
+    pub fn swapchain_image(mut self, swapchain_image: Rc<RefCell<DeviceResource>>) -> Self {
+        self.swapchain_image = Some(swapchain_image);
+        self
+    }
+
+    pub fn build(mut self) -> Result<PresentPassNode, &'static str> {
+        if let Some(swapchain_image) = self.swapchain_image {
+            Ok(PresentPassNode {
+                swapchain_image,
+                name: self.name
+            })
+        } else {
+            Err("PresentPassNode requires a swapchain image")
+        }
+    }
+}
+
+impl PassNode for PresentPassNode {
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    fn get_reads(&self) -> Vec<u64> {
+        vec![self.swapchain_image.borrow().get_handle()]
+    }
+
+    fn get_writes(&self) -> Vec<u64> {
+        vec![]
+    }
+
+    fn execute(&self, render_context: &mut VulkanRenderContext, command_buffer: &CommandBuffer) {
+        todo!()
+    }
+}
