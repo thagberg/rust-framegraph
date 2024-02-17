19bd8124cc8fabcf073a1d8939936ce49a8b1490
Tim Hagberg <timothy.m.hagberg@gmail.com>
2024-02-17 15:40:34 -0500

    Load gltf model

--- framegraph-examples/src/model_example.rs
+++ framegraph-examples/src/model_example.rs
@@ -1,10 +1,17 @@
 use imgui::Ui;
+use gltf::Gltf;
 use framegraph::attachment::AttachmentReference;
 use framegraph::pass_type::PassType;
 use crate::example::Example;
 
-pub struct ModelExample {
+pub struct GltfModel {
+    document: gltf::Document,
+    buffers: Vec<gltf::buffer::Data>,
+    images: Vec<gltf::image::Data>
+}
 
+pub struct ModelExample {
+    duck_model: GltfModel
 }
 
 impl Example for ModelExample {
@@ -14,5 +21,27 @@ impl Example for ModelExample {
 
     fn execute(&self, imgui_ui: &mut Ui, back_buffer: AttachmentReference) -> Vec<PassType> {
         Vec::new()
+    }
+}
+
+impl ModelExample {
+    pub fn new() -> Self {
+        let duck_import = gltf::import("assets/models/gltf/duck/Duck.gltf");
+        let duck_gltf = match duck_import {
+            Ok(gltf) => {
+                GltfModel {
+                    document: gltf.0,
+                    buffers: gltf.1,
+                    images: gltf.2
+                }
+            },
+            Err(e) => {
+                panic!("Failed to open Duck gltf model: {}", e)
+            }
+        };
+
+        ModelExample{
+            duck_model: duck_gltf
+        }
     }
 }
