524cd553ca9f1656980462158729d97594019ac2
thagberg <timothy.m.hagberg@gmail.com>
2024-05-18 23:12:09 -0400

    WIP: using camera and node transforms from gltf documents

--- util/src/camera.rs
+++ util/src/camera.rs
@@ -28,4 +28,33 @@ impl Camera {
             )
         }
     }
+
+    pub fn new_from_view(
+        aspect: f32,
+        vertical_fov: f32,
+        near: f32,
+        far: f32,
+        view: glm::TMat4<f32>) -> Self {
+
+        let proj = glm::Mat4::from_columns(&[
+            glm::Vec4::new(1.0 / (aspect * (0.5 * vertical_fov).tan()), 0.0, 0.0, 0.0),
+            glm::Vec4::new(0.0, 1.0 / (0.5 * vertical_fov).tan(), 0.0, 0.0),
+            glm::Vec4::new(0.0, 0.0, (far + near) / (near - far), ((2.0 * far * near))/ (near - far)),
+            glm::Vec4::new(0.0, 0.0, -1.0, 0.0)
+        ]).transpose();
+        Camera {
+            // projection: glm::perspective(
+            //     aspect,
+            //     vertical_fov,
+            //     near,
+            //     far
+            // ),
+            projection: proj,
+            view
+        }
+    }
+
+    pub fn get_view(&self) -> glm::Mat4 {
+        self.view.try_inverse().unwrap()
+    }
 }
