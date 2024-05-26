f4c5e7862ee864b5563125a20ef89d04252f5062
thagberg <timothy.m.hagberg@gmail.com>
2024-05-26 00:34:17 -0400

    Cleanup camera projection creation

--- util/src/camera.rs
+++ util/src/camera.rs
@@ -13,6 +13,7 @@ impl Camera {
         eye: &glm::TVec3<f32>,
         center: &glm::TVec3<f32>,
         up: &glm::TVec3<f32>) -> Self {
+
         Camera {
             projection: glm::perspective(
                 aspect,
@@ -36,20 +37,13 @@ impl Camera {
         far: f32,
         view: glm::TMat4<f32>) -> Self {
 
-        let proj = glm::Mat4::from_columns(&[
-            glm::Vec4::new(1.0 / (aspect * (0.5 * vertical_fov).tan()), 0.0, 0.0, 0.0),
-            glm::Vec4::new(0.0, 1.0 / (0.5 * vertical_fov).tan(), 0.0, 0.0),
-            glm::Vec4::new(0.0, 0.0, (far + near) / (near - far), ((2.0 * far * near))/ (near - far)),
-            glm::Vec4::new(0.0, 0.0, -1.0, 0.0)
-        ]).transpose();
         Camera {
-            // projection: glm::perspective(
-            //     aspect,
-            //     vertical_fov,
-            //     near,
-            //     far
-            // ),
-            projection: proj,
+            projection: glm::perspective(
+                aspect,
+                vertical_fov,
+                near,
+                far
+            ),
             view
         }
     }
