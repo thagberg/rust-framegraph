76ac014abc9b886e6ce64d2df5a15f7bdb0de959
thagberg <timothy.m.hagberg@gmail.com>
2024-08-07 00:41:09 -0400

    Move enter_span! from util to context

--- context/src/lib.rs
+++ context/src/lib.rs
@@ -1,2 +1,14 @@
 pub mod vulkan_render_context;
 pub mod render_context;
+
+#[macro_export]
+macro_rules! enter_span {
+    ($level:expr, $name:expr, $($fields:tt)*) => {
+        let span = tracing::span!($level, $name, $($fields)*);
+        let _enter = span.enter();
+    };
+
+    ($level:expr, $name:expr) => {
+        enter_span!($level, $name,)
+    };
+}
--- util/src/lib.rs
+++ util/src/lib.rs
@@ -4,18 +4,6 @@ pub mod image;
 
 extern crate nalgebra_glm as glm;
 
-#[macro_export]
-macro_rules! enter_span {
-    ($level:expr, $name:expr, $($fields:tt)*) => {
-        let span = tracing::span!($level, $name, $($fields)*);
-        let _enter = span.enter();
-    };
-
-    ($level:expr, $name:expr) => {
-        enter_span!($level, $name,)
-    };
-}
-
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
