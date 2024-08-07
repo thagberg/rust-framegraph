f1eab00401d41e0eb590464ca03079900afd3d11
thagberg <timothy.m.hagberg@gmail.com>
2024-08-07 00:10:20 -0400

    Framegraph instrumenting and add convenience macro for starting and entering span

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -4,6 +4,18 @@ pub mod image;
 
 extern crate nalgebra_glm as glm;
 
+#[macro_export]
+macro_rules! enter_span {
+    ($level:expr, $name:expr, $($fields:tt)*) => {
+        let span = tracing::span!($level, $name, $($fields)*);
+        let _enter = span.enter();
+    };
+
+    ($level:expr, $name:expr) => {
+        enter_span!($level, $name,)
+    };
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
