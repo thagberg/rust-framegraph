ed77e0a9007ba07b5148c706aa072ef091079e79
thagberg <timothy.m.hagberg@gmail.com>
2025-08-16 16:33:33 -0400

    Update blit to use new device refactor

--- passes/src/blit.rs
+++ passes/src/blit.rs
@@ -1,8 +1,10 @@
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 use ash::vk;
 use glam::IVec2;
-use api_types::device::{DeviceResource, ResourceType};
+use api_types::device::resource::DeviceResource;
+use api_types::device::interface::DeviceInterface;
 
 use context::render_context::RenderContext;
 use context::vulkan_render_context::VulkanRenderContext;
@@ -10,52 +12,29 @@ use framegraph::copy_pass_node::CopyPassNode;
 use framegraph::pass_type::PassType;
 use profiling::{enter_gpu_span, enter_span};
 
-pub fn generate_pass(
-    source: Rc<RefCell<DeviceResource>>,
+pub fn generate_pass<'d>(
+    source: Arc<Mutex<DeviceResource<'d>>>,
     source_layer: u32,
-    dest: Rc<RefCell<DeviceResource>>,
+    dest: Arc<Mutex<DeviceResource<'d>>>,
     dest_layer: u32,
-    offsets: [IVec2; 2]) -> PassType {
+    offsets: [IVec2; 2]) -> PassType<'d> {
 
     let pass_node = CopyPassNode::builder("blit".to_string())
         .copy_src(source.clone())
         .copy_dst(dest.clone())
         .fill_commands(Box::new(
-            move |render_ctx: &VulkanRenderContext,
-                    command_buffer: &vk::CommandBuffer| {
+            move |device: &DeviceInterface,
+                    command_buffer: vk::CommandBuffer| {
 
                 enter_span!(tracing::Level::TRACE, "Blit");
-                let device = render_ctx.get_device();
-                let borrowed_device = device.borrow();
-                enter_gpu_span!("Blit GPU", "Passes", borrowed_device.get(), command_buffer, vk::PipelineStageFlags::ALL_GRAPHICS);
+                enter_gpu_span!("Blit GPU", "Passes", device.get(), &command_buffer, vk::PipelineStageFlags::ALL_GRAPHICS);
 
                 unsafe {
-                    let resolved_source = source.borrow();
-                    let resolved_dest = dest.borrow();
+                    let resolved_source = source.lock().unwrap();
+                    let resolved_dest = dest.lock().unwrap();
 
-                    let source_image = {
-                        if let Some(s) = &resolved_source.resource_type {
-                            if let ResourceType::Image(s) = s {
-                                s
-                            } else {
-                                panic!("Image expected as source for blit");
-                            }
-                        } else {
-                            panic!("Source is an unresolved resource for blit");
-                        }
-                    };
-
-                    let dest_image = {
-                        if let Some(d) = &resolved_dest.resource_type {
-                            if let ResourceType::Image(d) = d {
-                                d
-                            } else {
-                                panic!("Image expected as dest for blit");
-                            }
-                        } else {
-                            panic!("Dest is an unresolved resource for blit");
-                        }
-                    };
+                    let source_image = resolved_source.get_image();
+                    let dest_image = resolved_dest.get_image();
 
                     let offsets = [
                         vk::Offset3D::builder().x(offsets[0].x).y(offsets[0].y).z(0).build(),
@@ -76,8 +55,8 @@ pub fn generate_pass(
                         .dst_subresource(*dest_layer)
                         .src_offsets(offsets)
                         .dst_offsets(offsets);
-                    render_ctx.get_device().borrow().get().cmd_blit_image(
-                        *command_buffer,
+                    device.get().cmd_blit_image(
+                        command_buffer,
                         source_image.image,
                         vk::ImageLayout::TRANSFER_SRC_OPTIMAL,
                         dest_image.image,
