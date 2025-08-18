0afe53817d4eda7ab31d58a7d0f07404e3980059
thagberg <timothy.m.hagberg@gmail.com>
2025-08-18 17:32:11 -0400

    Missing lifetime specifier in clear

--- passes/src/clear.rs
+++ passes/src/clear.rs
@@ -13,7 +13,7 @@ use profiling::{enter_gpu_span, enter_span};
 
 pub fn clear<'d>(
     device: &'d DeviceInterface,
-    target: Arc<Mutex<DeviceResource>>,
+    target: Arc<Mutex<DeviceResource<'d>>>,
     aspect_mask: vk::ImageAspectFlags) -> PassType<'d>{
 
     let target_binding = ResourceBinding {
